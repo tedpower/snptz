437fcf703a29f6783450afde1fe51e508f8e7e7a
ewheeler <evanmwheeler@gmail.com>
2011-01-11 18:24:35 -0500

    refactored digest stuff for testing

---
+++ send_digest.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+
+import logging
+import datetime
+from google.appengine.api import mail
+from google.appengine.api import users
+from google.appengine.ext import db
+import models
+
+logging.info('Scheduled task ran.')
+
+def construct_digest(profile_list):
+    personalized_digest_plaintext = '''
+        Hi %(username)s,
+
+        Here's what your esteemed colleagues are up to this week:
+
+
+    '''
+
+    for profile in profile_list:
+        template = '''
+        %(colleague_nick)s (%s(colleague_teams)s)
+        ---------------------
+        %(colleague_tasks)s
+
+        '''
+        prof_name = profile.nickname
+        prof_team_names = ", ".join([m.team.name for m in profile.membership_set])
+        prof_taskweek = profile.this_weeks_taskweek
+        if len(prof_tasks) == 0:
+            prof_tasks = "just chillin... (no reported tasks!)"
+        else:
+            prof_tasks = "\n".join(prof_taskweek)
+
+        personalized_digest_plaintext = personalized_digest_plaintext +\
+            template % {"colleague_nick": prof_name,
+                        "colleague_teams": prof_team_names,
+                        "colleague_tasks": prof_tasks}
+    return personalized_digest_plaintext
+
+user_list = []
+user_list.append(models.Profile.find_by_email('evanmwheeler@gmail.com')
+user_list.append(models.Profile.find_by_email('tedpower@gmail.com')
+
+for user in user_list:
+
+    esteemed_colleages = user.esteemed_colleagues
+    if esteemed_colleagues is not None:
+        digest_message_body = construct_digest(esteemed_colleagues) % {"username": first_name}
+
+        digest = mail.EmailMessage(
+        sender='SNPTZ Esteemed Colleagues <digest@snptz.com>',
+        to=user.email,
+        reply_to='SNPTZ <mail@snptzapp.appspotmail.com>',
+        # TODO make the subject of the email include the date
+        subject='SNPTZ Esteemed Colleagues digest for %s' % datetime.datetime.now().strftime("%b %d"),
+        body=personalized_digest_plaintext)
+
+        digest.send()
