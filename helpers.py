89d67425fa5a8fa4543a3b259e922c58337af08b
ewheeler <evanmwheeler@gmail.com>
2011-02-23 23:52:05 -0500

    use hashtags not brackets

--- helpers.py
+++ helpers.py
@@ -17,34 +17,16 @@ def slugify(value):
     return (re.sub('[-\s]+', '-', value))
 
 def extract_tags(text):
-    ''' Given a string, this method extracts tags from within
-        brackets, returning a tag-and-bracket-free string and
-        a list of tags-sans-brackets.
-        e.g., "[ux, android app ] work on prototype of settings interface "
-            returns "work on prototype of settings interface"
-                    and ['ux','android-app'] '''
+    ''' Given a string, this method extracts hashtags
+        and returns a tuple of string-sans-hashtags and
+        a list of extracted tags.'''
     t = text
-    tag_list = None
-    tag_pattern = re.compile(r'\[(.*)\]')
-    # search item from email for tags (stuff between [])
-    matches = re.search(tag_pattern, t)
+    tag_list = []
+    tag_pattern = re.compile(r'\#([-A-Za-z0-9]+)')
+    matches = re.findall(tag_pattern, t)
     if matches is not None:
-        # if there are tags, cast the matches.groups() tuple
-        # as a list, and split each list item by commas and
-        # finally flatten the resulting list of lists
-        raw_tags = itertools.chain.from_iterable([m.split(',')
-            for m in list(matches.groups())])
-        # slugify all the tags, so 'my tag ' becomes 'my-tag'
-        tags = [slugify(rt) for rt in raw_tags]
-
-        # add list of slugified tags to Task's tags property
-        tag_list = tags
-        tag_w_brackets_pattern = re.compile(r'\[.*\]')
-        # replace tags (and enclosing brackets) with an
-        # empty string, and strip any leading or trailing
-        # whitespace from the task text
-        text_wo_tags = tag_w_brackets_pattern.sub("", t).strip()
-        # and add to Task's text property
-        t = text_wo_tags
-        return t, tag_list
+        for match in matches:
+            tag_list.append(match)
+        t_no_tags, num = re.subn(tag_pattern, "", t)
+        return t_no_tags.strip(), tag_list
     return t, tag_list
